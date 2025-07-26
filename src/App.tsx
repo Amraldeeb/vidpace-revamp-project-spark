9f541d770417a58db06459d6806ca0943004a112
Amr Aldeeb <deebvision.tm@gmail.com>
2025-07-26 14:02:54 +0400

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Schedule from "./pages/Schedule";
 import NotFound from "./pages/NotFound";
 import { Contact } from "./components/Contact";
-import { useEffect } from "react";
+
 
 const queryClient = new QueryClient();
 
@@ -29,4 +34,3 @@ const App = () => {
 };
 
 export default App;
-
